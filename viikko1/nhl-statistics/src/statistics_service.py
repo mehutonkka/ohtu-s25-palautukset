e8cac648cf48304056b7b2c41157411f97c90160
mehutonkka <mishka.hagelberg@gmail.com>
2025-11-01 22:17:31 +0200

    konstruktiot

--- viikko1/nhl-statistics/src/statistics_service.py
+++ viikko1/nhl-statistics/src/statistics_service.py
@@ -1,10 +1,10 @@
-from player_reader import PlayerReader
+#from player_reader import PlayerReader
 
 
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, reader):
+        #reader = PlayerReader()
 
         self._players = reader.get_players()
 
